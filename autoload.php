e602ac01a0029445fb3a30f0b7356423e06b783f
DQNEO <dqneoo@gmail.com>
2015-05-03 02:05:33 +0900

    eliminate Ethna_Generator

--- autoload.php
+++ autoload.php
@@ -19,8 +19,7 @@ spl_autoload_register(function($className){
     //単純に_区切りをディレクトリ区切りにマッピングする
     if (
         strpos($className, 'Ethna_Plugin_Subcommand') === 0 ||
-        strpos($className, 'Ethna_Plugin_Generator') === 0 ||
-        strpos($className, 'Ethna_Generator') === 0
+        strpos($className, 'Ethna_Plugin_Generator') === 0
     ) {
         $separated = explode('_', $className);
         array_shift($separated);  // remove first element
