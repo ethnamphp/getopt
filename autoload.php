915be22db30f4459b79d157bb1f4c8c7b6e5c4d0
DQNEO <dqneoo@gmail.com>
2015-05-02 16:03:10 +0900

    catch Ethna_Plugin_Subcommand

--- autoload.php
+++ autoload.php
@@ -4,7 +4,9 @@
  */
 spl_autoload_register(function($className){
     //単純に_区切りをディレクトリ区切りにマッピングする
-    if (strpos($className, 'Ethna_Command') === 0) {
+    if (strpos($className, 'Ethna_Command') === 0 ||
+        strpos($className, 'Ethna_Plugin_Subcommand') === 0
+    ) {
         $separated = explode('_', $className);
         array_shift($separated);  // remove first element
         //読み込み失敗しても死ぬ必要はないのでrequireではなくincludeする
