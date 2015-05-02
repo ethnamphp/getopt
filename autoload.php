dd83aa1f5381c011af09b85a72e23e335c0052b9
DQNEO <dqneoo@gmail.com>
2015-05-03 00:37:50 +0900

    require Plugin/Generator

--- autoload.php
+++ autoload.php
@@ -6,6 +6,7 @@ spl_autoload_register(function($className){
     //単純に_区切りをディレクトリ区切りにマッピングする
     if (strpos($className, 'Ethna_Command') === 0 ||
         strpos($className, 'Ethna_Plugin_Subcommand') === 0 ||
+        strpos($className, 'Ethna_Plugin_Generator') === 0 ||
         strpos($className, 'Ethna_Generator') === 0
     ) {
         $separated = explode('_', $className);
