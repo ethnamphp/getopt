9cb22781290f9d88d653de8333be343340c10717
DQNEO <dqneoo@gmail.com>
2015-05-02 16:37:19 +0900

    use Ethna_Generator

--- autoload.php
+++ autoload.php
@@ -5,7 +5,8 @@
 spl_autoload_register(function($className){
     //単純に_区切りをディレクトリ区切りにマッピングする
     if (strpos($className, 'Ethna_Command') === 0 ||
-        strpos($className, 'Ethna_Plugin_Subcommand') === 0
+        strpos($className, 'Ethna_Plugin_Subcommand') === 0 ||
+        strpos($className, 'Ethna_Generator') === 0
     ) {
         $separated = explode('_', $className);
         array_shift($separated);  // remove first element
