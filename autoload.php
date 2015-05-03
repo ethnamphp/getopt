953c174d062248e84ec32740c7fc2e58181eb49e
DQNEO <dqneoo@gmail.com>
2015-05-04 01:40:41 +0900

    cosmetic

--- autoload.php
+++ autoload.php
@@ -3,18 +3,18 @@
  *  オートロード
  */
 spl_autoload_register(function($className){
-    //単純に_区切りをディレクトリ区切りにマッピングする
     if (strpos($className, 'Ethnam\\Generator\\') === 0) {
         $separated = explode('\\', $className);
-        //var_dump($separated);exit;
         array_shift($separated);  // remove 'Ethnam'
         array_shift($separated);  // remove 'Generator'
 
         $file = __DIR__ . '/src/' . join('/', $separated) . '.php';
+
+	//just for debug
         if (!file_exists($file)) {
-            //just for debug
             throw new Exception("classs not found: " . $className);
         }
+
         require_once $file;
     }
 });
