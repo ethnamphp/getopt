3097eef7f419148de465dae6b4e4d6662c9d43a3
DQNEO <dqneoo@gmail.com>
2015-05-03 13:58:53 +0900

    fix comment

--- src/Plugin/Generator/Base.php
+++ src/Plugin/Generator/Base.php
@@ -28,7 +28,7 @@ class Ethna_Plugin_Generator_Base
      */
     function __construct($controller, $type, $name)
     {
-        // Ethna_Generatorでpluginを取得するときに使ったコントローラ
+        // Ethna_Plugin_Subcommand_Base::generateでpluginを取得するときに使ったコントローラ
         // ex, add-projectではEthna_Controller, app-actionではApp_Controller
         $this->ctl = $controller;
     }
