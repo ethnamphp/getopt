75e3cfd3dfc52b8854c4a906a6b443a5ca4146d2
DQNEO <dqneoo@gmail.com>
2015-05-03 17:44:11 +0900

    simplify autload

--- autoload.php
+++ autoload.php
@@ -14,17 +14,3 @@ spl_autoload_register(function($className){
         require_once __DIR__ . '/src/' . join('/', $separated) . '.php';
     }
 });
-
-spl_autoload_register(function($className){
-    //単純に_区切りをディレクトリ区切りにマッピングする
-    if (
-        strpos($className, 'Ethna_Subcommand') === 0 ||
-        strpos($className, 'Ethna_Generator') === 0
-    ) {
-        $separated = explode('_', $className);
-        array_shift($separated);  // remove first element
-        //読み込み失敗しても死ぬ必要はないのでrequireではなくincludeする
-        //see http://qiita.com/Hiraku/items/72251c709503e554c280
-        include_once __DIR__ . '/src/' . join('/', $separated) . '.php';
-    }
-});
